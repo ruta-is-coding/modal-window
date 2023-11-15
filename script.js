e6bc84444cdcd2cc73f851bbddfe23f0bf32664d
ruta-is-coding <rutajurg.123@gmail.com>
2023-11-15 10:15:20 +0200

    Selected elements, added functions and onclick events

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+//Selected elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+//Functions
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Displaying the modal window and the overlay
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', openModal);
+
+//Hiding the modal window and the overlay
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
